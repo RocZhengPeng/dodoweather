a335c567014ae60cbf94637435379f1356a885b2
zhengpeng <306608923@qq.com>
2017-07-14 17:45:37 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/dodoweather/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.dodoweather.android.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
